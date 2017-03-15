1d198e1c68602d310d8c52332d805f3b8bff54c5
michaelciletti <michaelciletti@gmail.com>
2017-03-15 16:38:24 -0400

    a few more passing tests

--- board.rb
+++ board.rb
@@ -13,4 +13,13 @@ attr_accessor :tttboard
 	def open_space?(position)
 		tttboard[position] == ""
 	end
+
+	def full_board?()
+		tttboard.count("") == 0
+	end
+
+	def board_win?(marker)
+		tttboard[0] == marker && tttboard[1] == marker && tttboard[2] == marker 
+	end
+
 end
--- test_board.rb
+++ test_board.rb
@@ -47,5 +47,29 @@ class TestBoard < Minitest::Test
 	assert_equal(false,board.open_space?(0))
 	end
 
+	def test_invalid_position
+	board = Board.new 
+	board.tttboard = ["x", "x", "", "o", "o", "", "", "x", "o"]
+	assert_equal(false,board.open_space?(11))
+	end
+
+	def test_full_board
+	board = Board.new 
+	board.tttboard = ["x", "x", "x", "o", "o", "x", "x", "x", "x"]
+	assert_equal(true,board.full_board?())
+	end
+
+	def test_nonfull_board
+	board = Board.new 
+	board.tttboard = ["x", "x", "x", "o", "o", "x", "x", "x", ""]
+	assert_equal(false,board.full_board?())
+	end
+
+	def test_win_x
+	board = Board.new 
+	board.tttboard = ["x", "x", "x", "", "", "", "", "", ""]
+	marker = "x"
+	assert_equal(true,board.board_win?(marker))
+	end
 
 end
