66b0f43924a59c511830b6cc6288ab1257e54d91
michaelciletti <michaelciletti@gmail.com>
2017-03-28 15:59:35 -0400

    fixing some tests that now fail and have errors due to changes in functionality

--- test_board.rb
+++ test_board.rb
@@ -10,7 +10,7 @@ class TestBoard < Minitest::Test
 	def test_update_with_x_at_position_1
 		board = Board.new 
 		board.update_board(0,"x")
-		assert_equal(["x", "", "", "", "", "", "", "", ""], board.tttboard)
+		assert_equal(["x", "2", "3", "4", "5", "6", "7", "8", "9"], board.tttboard)
 	end
 
 	def test_update_with_x_at_position_1_o_at_position_9
